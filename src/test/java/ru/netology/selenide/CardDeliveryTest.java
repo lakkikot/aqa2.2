00a0e2ac22638fde1e689c83586bfd6ee4897a5d
Natalya Makarevich <mak.nathalie@gmail.com>
2021-10-28 22:31:51 +0300

    Add v2 test

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -1,13 +1,12 @@
 package ru.netology.selenide;
 
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ex.ElementNotFound;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.Duration;
-import java.time.LocalDate;
+import java.text.ParseException;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
@@ -25,9 +24,9 @@ public class CardDeliveryTest {
     void ShouldSendForm() {
         //готовим дату
         String rightDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
         $("[data-test-id=city] input").setValue("Саратов");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE, rightDate);
-        //   $("[data-test-id='date']").setValue(String.valueOf(LocalDate.now().plusDays(4)));
         $("[name='name']").setValue("Василий");
         $("[name='phone']").setValue("+79991234567");
         $(withText("Я соглашаюсь с условиями обработки и использования моих персональных данных")).click();
@@ -36,25 +35,31 @@ public class CardDeliveryTest {
     }
 
 
-    //$$("[class='menu-item__control']")
-
     @Test
-    void ShouldSendFormV2() {
-        //готовим дату
-        String rightDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    void ShouldSendFormV2() throws ParseException {
+
         $("[data-test-id=city] input").setValue("Са");
-        $$("[class='menu-item__control']").last().click();
-        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
+        $$("[class='menu-item__control']").last().click(); //клик по последнему в списке
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE); //очистить поле ввода даты
         $(".icon_name_calendar").click();
-        $("[data-step='1']").click();
-        $("tr:nth-child(3) > td:nth-child(2)").click();
+
+        //дата через неделю в unix-формате - как дни в календаре
+        String unixDate = String.valueOf(LocalDate.now().atStartOfDay(ZoneId.of("GMT+3")).plusDays(7).toInstant().toEpochMilli());
+
+        //проверяем, есть ли дата на следующей строке на том же месте
+        try {
+            $("tr > td.calendar__day.calendar__day_type_off.calendar__day_state_today+tr").click();
+        } catch (ElementNotFound e) {
+            $("[data-step='1']").click(); //если не оказалось, то кликаем на след месяц
+            $("[data-day='" + unixDate + "']").click(); //ищем нужную unix-дату
+        }
+
         $("[name='name']").setValue("Василий");
         $("[name='phone']").setValue("+79991234567");
         $(withText("Я соглашаюсь с условиями обработки и использования моих персональных данных")).click();
         $(withText("Забронировать")).click();
         $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
     }
-
 
 
 }
